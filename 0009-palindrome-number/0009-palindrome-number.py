c4b392818044e799f38784bfab332a1fae63c898
1HD22 <103599841+1HD22@users.noreply.github.com>
2022-11-02 14:46:47 +0800

    Time: 126 ms (49.57%), Space: 13.9 MB (16.36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        
+        for i in range(len(x)//2):
+            if x[i] == x[-i-1]:
+                continue
+            else:
+                return False
+        return True
