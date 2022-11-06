8be498b1d419597294e58bd396b12dd9d199faaa
1HD22 <103599841+1HD22@users.noreply.github.com>
2022-11-06 18:58:39 +0800

    Time: 58 ms (38.04%), Space: 13.9 MB (59.63%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -3,11 +3,11 @@ class Solution:
         
         if (low % 2 and high % 2):
             return int((high - low)/2 + 1)
-        elif (not(low % 2) and high % 2):
+        elif (low % 2 == 0 and high % 2):
             return int((high - low - 1) / 2 + 1)
-        elif (low % 2 and not(high % 2)):
+        elif (low % 2 and high % 2 == 0):
             return int((high - low - 1) / 2 + 1)
-        elif (not(low % 2) and not(high % 2)):
+        elif (low % 2== 0 and high % 2 == 0):
             return int((high - low) / 2)
         
         return -1
