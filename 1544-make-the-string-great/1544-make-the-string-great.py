2368fe297b57f46781f51d6436011179a3badd67
1HD22 <103599841+1HD22@users.noreply.github.com>
2022-11-08 22:07:43 +0800

    Time: 62 ms (65.95%), Space: 13.8 MB (61.93%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -5,14 +5,11 @@ class Solution:
         return False
     
     def makeGood(self, s: str) -> str:
-        t = len(s) - 1
         i = 0
-        count = 0
         
-        while(i < t):
+        while(i < len(s) - 1):
             if (self.isbad(s[i],s[i+1])):
                 s = s[:i] + s[i+2:]
-                t -= 2
                 i = -1
             i += 1
             
