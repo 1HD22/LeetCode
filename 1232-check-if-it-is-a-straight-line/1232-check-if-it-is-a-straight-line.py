ec1fb46424ded15831175c3f4c3424e12750d5e5
1HD22 <103599841+1HD22@users.noreply.github.com>
2022-11-11 00:09:40 +0800

    Time: 155 ms (11.65%), Space: 14.5 MB (31.34%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -0,0 +1,15 @@
+class Solution:
+    def verticleLine(self, coordinates: List[List[int]]) -> bool:
+        for j in range(len(coordinates) - 1):
+            if coordinates[j][0] != coordinates[j+1][0]:
+                return False
+        return True
+        
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:        
+        for i in range(1,len(coordinates)):
+            if (coordinates[i][0] == coordinates[0][0]):
+                return self.verticleLine(coordinates)
+                 
+            if ((coordinates[i][1] - coordinates[0][1])/ (coordinates[i][0] - coordinates[0][0]) != (coordinates[1][1] - coordinates[0][1])/ (coordinates[1][0] - coordinates[0][0])):
+                return False
+        return True
