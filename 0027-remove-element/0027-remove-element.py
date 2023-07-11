235fcdb9318f220a61af631571faa2a501411a76
1HD22 <103599841+1HD22@users.noreply.github.com>
2023-07-11 15:23:04 +0800

    Time: 54 ms (29.07%), Space: 16.3 MB (35.94%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,14 +1,31 @@
 class Solution:
+    def removeStack(self, index: int) -> None:
+        if index == 0:
+            self.length -= 1
+            self.nums[:self.length] = self.nums[1:self.length+1]
+            
+        elif index == len(self.nums)-1:
+            self.length -= 1
+        
+        else:
+            self.length -= 1
+            self.nums[index:self.length] = self.nums[index+1:self.length+1]
+            
+        
     def removeElement(self, nums: List[int], val: int) -> int:
-        count = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                count = count + 1
-            if nums[i] == val:
-                for j in range(i+1,len(nums)):
-                    if nums[j] != val:
-                        count = count + 1
-                        nums[i] = nums[j]
-                        nums[j] = val
-                        break
-        return count
+        self.nums = nums
+        self.length = len(nums)
+        self.val = val
+        
+        i = 0
+        while i < self.length:
+            
+            if self.nums[i] == val:
+                self.removeStack(i)
+            else:
+                i += 1
+
+        nums = self.nums
+        return self.length
+
+        
