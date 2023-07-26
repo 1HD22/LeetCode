02d6b4bd66422a572e4fc595151296dcbfe932e4
1HD22 <103599841+1HD22@users.noreply.github.com>
2023-07-26 16:27:56 +0800

    Time: 180 ms (40.81%), Space: 17.7 MB (96.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -3,17 +3,33 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
        # initial naive solution. Time: O(n), Space: O(n)
-        count = {}
-        length = len(nums)
+#         count = {}
+#         length = len(nums)
         
-        for i in range(length):
-            if nums[i] not in count.keys():
-                count[nums[i]] = 1
+#         for i in range(length):
+#             if nums[i] not in count.keys():
+#                 count[nums[i]] = 1
+#             else:
+#                 count[nums[i]] += 1
+            
+#             if count[nums[i]] > length // 2:
+#                 return nums[i]
+            
+        
+#         return -1
+
+        # Boyer-Moore Majority Voting. Time: O(n), Space: O(1)
+        majority = -1
+        i = 0
+        
+        for x in nums:
+            if i == 0:
+                m=x
+                i=1
+            elif x == m:
+                i += 1
             else:
-                count[nums[i]] += 1
-            
-            if count[nums[i]] > length // 2:
-                return nums[i]
-            
+                i -= 1
         
-        return -1
+        return m
+        
