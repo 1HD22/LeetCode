2db3035874117731060407788ef4cbc9c89cd4de
1HD22 <103599841+1HD22@users.noreply.github.com>
2023-07-28 00:32:25 +0800

    Time: 1361 ms (61.22%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+#  Write your MySQL query statement below
 SELECT query_name, round(avg(rating/position),2) AS quality, 
     round(sum(CASE WHEN rating<3 THEN 1 ELSE 0 END) * 100 / count(rating), 2) AS poor_query_percentage
 FROM Queries
