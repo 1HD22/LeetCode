0ce22808a35db5b32473f1654fd03f16f2154e8a
1HD22 <103599841+1HD22@users.noreply.github.com>
2023-07-27 17:18:55 +0800

    Time: 348 ms (96.50%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+#  Write your MySQL query statement below
 SELECT id, movie, description, rating
 FROM Cinema
 WHERE MOD(id, 2) = 1 AND description != "boring"
