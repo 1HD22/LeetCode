d54b1bcc1e5c0f6568972c207d5a7764395c04c1
1HD22 <103599841+1HD22@users.noreply.github.com>
2023-07-27 14:33:20 +0800

    Time: 469 ms (60.42%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -3,5 +3,5 @@ SELECT a1.machine_id, round(avg(a2.timestamp-a1.timestamp),3) as "processing_tim
 FROM Activity as a1
 INNER JOIN Activity as a2 
 ON a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id 
-AND a1.activity_type = "start" AND a2.activity_type = "end"
+AND a1.activity_type > a2.activity_type
 GROUP BY machine_id
