3850cecfdd684dcef5387dd52475b413771d09b0
1HD22 <103599841+1HD22@users.noreply.github.com>
2023-07-29 12:33:03 +0800

    Time: 530 ms (74.76%), Space: 0B (100.00%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,4 +1,4 @@
-#  Write your MySQL query statement below
+# Write your MySQL query statement below
 SELECT x, y, z, (CASE WHEN
                 x+y>z AND x+z>y AND y+z>x
                 THEN "Yes"
