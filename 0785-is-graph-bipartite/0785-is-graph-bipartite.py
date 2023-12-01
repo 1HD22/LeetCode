76a4b3a72f2bad5377fe58b1b6383e215d6dc050
1HD22 <103599841+1HD22@users.noreply.github.com>
2023-12-01 15:56:31 +0800

    Time: 172 ms (21.20%), Space: 17 MB (27.59%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,43 @@
+class Solution:
+    def __init__(self):
+        self.grapho = []
+    class node:
+        def __init__(self, id):
+            self.id = id
+            self.color = 0
+            self.d = 0
+            self.p = None
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        for i in range(len(graph)):
+            self.grapho.append(self.node(i))
+        
+        for u in self.grapho:
+            if u.color == 0:
+                self.BFSVisit(u, graph)
+                
+        print([(u.id, u.d, (lambda x: u.p.id if u.p is not None else None)(u)) for u in self.grapho])
+        
+        for u in self.grapho:
+            for v in graph[u.id]:
+                if u.d % 2 == self.grapho[v].d % 2:
+                    return False
+        
+        return True
+    
+    
+    def BFSVisit(self, s, graph):
+        s.color = 1
+        s.d = 0
+        Q = []
+        Q.append(s)
+        while len(Q) != 0:
+            u = Q.pop(0)
+            for v in graph[u.id]:
+                if self.grapho[v].color == 0:
+                    self.grapho[v].color = 1
+                    self.grapho[v].d = u.d + 1
+                    self.grapho[v].p = u
+                    Q.append(self.grapho[v])
+            
+            u.color = 2
+        
