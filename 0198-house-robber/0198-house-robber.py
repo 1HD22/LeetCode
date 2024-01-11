98116a3cecffcf0d51ecb5e5d93680a7ff321815
1HD22 <103599841+1HD22@users.noreply.github.com>
2024-01-11 18:31:12 +0800

    Time: 38 ms (66.68%), Space: 17.3 MB (15.11%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,19 @@
+class Solution:
+    
+    def rob(self, nums: List[int]) -> int:
+        
+        larger = lambda a,b: a if a>b else b
+        n = len(nums)
+        
+        if n == 1:
+            return nums[0]
+        
+        arr = [0 for i in nums]
+        
+        arr[0] = nums[0]
+        arr[1] = larger(nums[0], nums[1])
+        
+        for i in range(2, len(nums)):
+            arr[i] = larger(arr[i-2]+nums[i], arr[i-1])
+            
+        return arr[n-1]
