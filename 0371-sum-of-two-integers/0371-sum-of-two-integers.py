84236c63f30724ad6bc1f211c6915f03d0fb77a2
1HD22 <103599841+1HD22@users.noreply.github.com>
2024-09-29 17:03:23 +0800

    Time: 37 ms (36.49%), Space: 16.6 MB (18.33%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        keep = (a & b) << 1
+        res = a^b
+        
+        if keep == 0:
+            return res
+        
+        return add(keep, res)
